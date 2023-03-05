3b7e5768d15159531445e04713f67ee4b07efa27
alexy <alexyshu89@gmail.com>
2023-03-05 22:23:19 +0300

    fixed homework-2 task-3

--- src/functions.php
+++ src/functions.php
@@ -17,4 +17,39 @@ function task1(array $array, bool $value = false) {
     return $strings;
 }
 
+// task 2
+// Функция должна принимать переменное число аргументов.
+// Первым аргументом обязательно должна быть строка, обозначающая арифметическое действие, которое необходимо выполнить со всеми передаваемыми аргументами.
+// Остальные аргументы это целые и/или вещественные числа.
+// Пример вызова: calcEverything(‘+’, 1, 2, 3, 5.2);
+// Результат: 1 + 2 + 3 + 5.2 = 11.2
+
+function task2(string $action, ...$args) {
+    switch($action) {
+        case "+":
+            return array_sum($args);
+            break;
+        case "-":
+            return array_shift($args) - array_sum($args);
+            break;
+        case "*":
+            $result = reset($args);
+            for($i = 1; $i < count($args); $i++) {
+                $result *= $args[$i];
+            }
+            return $result;
+            break;
+        case "/":
+            $result = reset($args);
+            for($i = 1; $i < count($args); $i++) {
+                $result /= $args[$i];
+            }
+            return $result;
+            break;
+            
+        default:
+            return"Неизвестный симфол" ;
+            break;
+    };
+}
 
--- src/index.php
+++ src/index.php
@@ -1,6 +1,11 @@
 <?php
 require('functions.php');
 
-echo task1(["test 1", "test 2", "test 3"], true);
-echo task1(["test 1", "test 2", "test 3"], false);
-echo PHP_EOL;
+//echo task1(["test 1", "test 2", "test 3"], true);
+// echo task1(["test 1", "test 2", "test 3"], false);
+// echo PHP_EOL;
+
+echo task2("+", 1, 2, 3) . PHP_EOL;
+echo task2("-", 1, 2, 3) . PHP_EOL;
+echo task2("*", 1, 2, 3) . PHP_EOL;
+echo task2("/", 1, 2, 3) . PHP_EOL;
