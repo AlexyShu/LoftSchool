de64ddadbfdbe5dd57f1e97d2c4db2fec5e0346b
alexy <alexyshu89@gmail.com>
2023-03-05 13:20:56 +0300

    fixed homework-1 task-4

---
+++ homework-1/task-4.php
@@ -0,0 +1,15 @@
+<?php
+$day = mt_rand(1, 7);
+echo "Сегодня $day день недели" . PHP_EOL;
+
+switch ($day) {
+    case ($day >= 1 && $day <= 5):
+        echo "Это рабочий день" . PHP_EOL;
+        break;
+    case ($day === 6 || $day === 7):
+        echo "Это выходной день" . PHP_EOL;
+        break;
+    default:
+        echo "Неизвестный день" . PHP_EOL;
+        break;
+}
