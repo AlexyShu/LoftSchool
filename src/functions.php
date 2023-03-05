948b0ccd08bab9e838f322be9c3ab303be8df40f
alexy <alexyshu89@gmail.com>
2023-03-05 19:30:10 +0300

    fixed homework-2 task-1

--- src/functions.php
+++ src/functions.php
@@ -1,1 +1,20 @@
 <?php
+
+// task 1
+// Функция должна принимать массив строк и выводить каждую строку в отдельном параграфе (тег <p>)
+// Если в функцию передан второй параметр true, то возвращать (через return) результат в виде одной объединенной строки.
+
+function task1(array $array, bool $value = false) {
+    if(!$value) {
+        $strings = implode(", ", $array);
+    return "<p> $strings</p>" . PHP_EOL;
+    } 
+
+    $strings = implode(", ", array_map( function(string $string) {
+        return "<p> $string</p>";
+    }, $array));
+
+    return $strings;
+}
+
+
--- src/index.php
+++ src/index.php
@@ -1,2 +1,6 @@
 <?php
-require("src/functions.php");
+require('functions.php');
+
+echo task1(["test 1", "test 2", "test 3"], true);
+echo task1(["test 1", "test 2", "test 3"], false);
+echo PHP_EOL;
