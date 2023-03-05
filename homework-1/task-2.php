a5343c28fc63dc51394af92f5c2f72304acc6678
alexy <alexyshu89@gmail.com>
2023-03-05 12:57:59 +0300

    fixed homework-1 task-2

--- homework-1/task-2.php
+++ homework-1/task-2.php
@@ -1,1 +1,7 @@
 <?php
+const ALL_PICTURE = 80;
+const PICTURES_BY_FELTTIP = 23;
+const PICTURES_BY_PENCIL = 40;
+$picturesByPaints = ALL_PICTURE - (PICTURES_BY_FELTTIP + PICTURES_BY_PENCIL);
+
+echo "Рисунков выполненных красками, на школьной выставке $picturesByPaints" . PHP_EOL;
