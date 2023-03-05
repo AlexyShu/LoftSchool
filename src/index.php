e3a0896e4fcf1c82b81f0cae3567cd84b6088456
alexy <alexyshu89@gmail.com>
2023-03-05 22:54:56 +0300

    fixed homework-2 task-3

--- src/index.php
+++ src/index.php
@@ -5,7 +5,9 @@ require('functions.php');
 // echo task1(["test 1", "test 2", "test 3"], false);
 // echo PHP_EOL;
 
-echo task2("+", 1, 2, 3) . PHP_EOL;
-echo task2("-", 1, 2, 3) . PHP_EOL;
-echo task2("*", 1, 2, 3) . PHP_EOL;
-echo task2("/", 1, 2, 3) . PHP_EOL;
+// echo task2("+", 1, 2, 3) . PHP_EOL;
+// echo task2("-", 1, 2, 3) . PHP_EOL;
+// echo task2("*", 1, 2, 3) . PHP_EOL;
+// echo task2("/", 1, 2, 3) . PHP_EOL;
+
+echo task3(5,7);
